7f398864b4eaac5566e25e8b1db3ef33be5235b7
Matt Strick <matt.strick@gmail.com>
2014-11-15 15:12:09 -0600

    Controls for changing show types are now in.

--- controls.js
+++ controls.js
@@ -9,35 +9,75 @@ var controls = (function() {
       "Friday",
       "Saturday"
     ],
-    days_form : "#day_controls"
+    days_form : "#day_controls",
+    showTypes : [
+      "solo",
+      "group",
+      "standup",
+      "festival"
+    ],
+    showTypes_form : "#show_type_controls"
   };
 
   // Function declarations
   var init;
 
   init = function() {
-    var days_checkboxes, any_day_checkbox, ind_day_checkbox;
+    var days_checkbox, any_day_checkbox, ind_day_checkbox;
 
     // Click handling of Days Form inputs.
-    days_checkboxes = $(CONFIGS.days_form + " input:checkbox");
+    days_checkbox = $(CONFIGS.days_form + " input:checkbox");
     any_day_checkbox = $(CONFIGS.days_form + " input[value='Any']");
-    // Returns the set of elements in days_checkboxes without the any_day_checkbox element
-    ind_day_checkbox = $($.grep(days_checkboxes, function(value) {  return value != any_day_checkbox.get(0);}));
+    // Returns the set of elements in days_checkbox without the any_day_checkbox element
+    ind_day_checkbox = $($.grep(days_checkbox, function(value) {  return value != any_day_checkbox.get(0);}));
+
+    // Click handling of Types Form inputs.
+    types_checkbox = $(CONFIGS.showTypes_form + " input:checkbox");
+    any_type_checkbox = $(CONFIGS.showTypes_form + " input[value='Any']");
+    // Returns the set of elements in types_checkbox without the any_type_checkbox element
+    ind_types_checkbox = $($.grep(types_checkbox, function(value) {  return value != any_type_checkbox.get(0);}));
 
 
+    /*
+      Form Update Handlers
+     */
+    // Update Day Controls - Any
     any_day_checkbox.on('click', function () {
-      days_checkboxes.prop('checked',false);
+      days_checkbox.prop('checked',false);
       any_day_checkbox.prop('checked',true);
     });
 
+    // Update Day Controls - Day 
     ind_day_checkbox.on('click', function () {
       any_day_checkbox.prop('checked',false);
     })
 
+    // Update Type Controls - Any
+    any_type_checkbox.on('click', function () {
+      types_checkbox.prop('checked',false);
+      any_type_checkbox.prop('checked',true);
+    });
+
+    // Update Type Controls - Type 
+    ind_types_checkbox.on('click', function () {
+      any_type_checkbox.prop('checked',false);
+    })
+
+
+    /*
+      Form Submit Handlers
+     */ 
+    
     // Submit handling of Days Form.
     $(CONFIGS.days_form).on('submit', function(e) {
       e.preventDefault();
       changeShowDays($(this).find('input:checked'));
+    });
+
+    // Submit handling of Types Form.
+    $(CONFIGS.showTypes_form).on('submit', function(e) {
+      e.preventDefault();
+      changeShowTypes($(this).find('input:checked'));
     });
 
   };
@@ -62,6 +102,27 @@ var controls = (function() {
       updateVisibleShows(day_arr);
     }
 
+  };
+
+  // changeShowTypes - converts a selection of DOM elements to an array of types that 
+  // updateVisibleShows can parse.
+  changeShowTypes = function(selected_types) {
+    if (typeof selected_types === undefined) {
+      console.error('No types passed.');
+    } else {
+      var type_arr = [];
+      selected_types.each(function() {
+        if ($(this).val() === 'Any') {
+          type_arr = [];
+          return;
+        } else {
+          type_arr.push('.'+$(this).val());
+        }
+      });
+
+      // Pass type_arr to update function
+      updateVisibleShows(type_arr);
+    }
   };
 
 
