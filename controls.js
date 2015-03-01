77d0f3a65ff86a08b10cc92d3eb5755be709fb0f
Matt Strick <matt.strick@gmail.com>
2015-03-01 00:11:54 -0600

    Better abstraction of the controls section. It isn't one long hard coded value anymore.

--- controls.js
+++ controls.js
@@ -4,23 +4,24 @@ var controls = (function() {
     container : '#controls',
     form : "#controls form",
     days : [
-      "Sunday",
       "Monday",
       "Tuesday",
       "Wednesday",
       "Thursday",
       "Friday",
-      "Saturday"
+      "Saturday",
+      "Sunday"
     ],
+    days_all : "daysAny",
     days_fieldset : "[name=day_controls]",
     showTypes : [
       "solo",
       "group",
       "standup",
       "sketch_g",
-      "sketch_s",
-      "festival"
+      "sketch_s"
     ],
+    showTypes_all : "showTypesAny",
     showTypes_fieldset : "[name=show_type_controls]"
   };
 
@@ -35,13 +36,13 @@ var controls = (function() {
 
     // Click handling of Days Form inputs.
     days_checkbox = $(CONFIGS.days_fieldset + " input:checkbox");
-    any_day_checkbox = $(CONFIGS.days_fieldset + " input[value='Any']");
+    any_day_checkbox = $(CONFIGS.days_fieldset + " input[value='"+ CONFIGS.days_all +"']");
     // Returns the set of elements in days_checkbox without the any_day_checkbox element
     ind_day_checkbox = $($.grep(days_checkbox, function(value) {  return value != any_day_checkbox.get(0);}));
 
     // Click handling of Types Form inputs.
     types_checkbox = $(CONFIGS.showTypes_fieldset + " input:checkbox");
-    any_type_checkbox = $(CONFIGS.showTypes_fieldset + " input[value='Any']");
+    any_type_checkbox = $(CONFIGS.showTypes_fieldset + " input[value='"+ CONFIGS.showTypes_all +"']");
     // Returns the set of elements in types_checkbox without the any_type_checkbox element
     ind_types_checkbox = $($.grep(types_checkbox, function(value) {  return value != any_type_checkbox.get(0);}));
 
@@ -79,13 +80,7 @@ var controls = (function() {
     $(CONFIGS.form).on('submit', function(e) {
       var updatedSelectors = [];
       e.preventDefault();
-      /*
-      // Submit handling of Days Form.
-      updatedSelectors += changeShowDays($(this).find(CONFIGS.days_fieldset + ' input:checked'));
-      // Submit handling of Types Form.
-      updatedSelectors += changeShowTypes($(this).find(CONFIGS.showTypes_fieldset + ' input:checked'));
-      console.log("Showing classes with selectors: " + updatedSelectors);
-      */
+
       updateVisibleShows(this);
     });
 
@@ -93,9 +88,73 @@ var controls = (function() {
   };
 
   addControls = function() {
+    // Add Form
     $(CONFIGS.container).append(
-      '<!-- <button class="add-show">Add Show</button>--><form><fieldset name="day_controls"><p>Filter by Days:</p><input type="checkbox" name="day" value="Any" checked />Any<input type="checkbox" name="day" value="Monday" />M<input type="checkbox" name="day" value="Tuesday" />T<input type="checkbox" name="day" value="Wednesday" />W<input type="checkbox" name="day" value="Thursday" />Th<input type="checkbox" name="day" value="Friday" />F<input type="checkbox" name="day" value="Saturday" />Sa<input type="checkbox" name="day" value="Sunday" />Su</fieldset><fieldset name="show_type_controls"><p>Filter by Type:</p><input type="checkbox" name="type" value="Any" checked />Any<input type="checkbox" name="type" value="solo" />Improv (Solo)<input type="checkbox" name="type" value="group" />Improv (Group)<input type="checkbox" name="type" value="standup" />Stand-up<input type="checkbox" name="type" value="sketch_s" />Sketch (Solo)<input type="checkbox" name="type" value="sketch_g" />Sketch (Group)<!--<input type="checkbox" name="type" value="festival" />Festival--></fieldset><input type="submit" value="Update Types" /></form><hr>'
+      '<!-- <button class="add-show">Add Show</button>-->'+
+      '<form>' +
+      '<fieldset name="day_controls"><p>Filter by Days:</p></fieldset>' +
+      '<fieldset name="show_type_controls"><p>Filter by Type:</p></fieldset>' +
+      '<input type="submit" value="Update Types" />' +
+      '</form><hr>'
     );
+
+    // Day inputs
+    // Build the inputs
+    _days = "";
+    _days += buildInput(CONFIGS.days_all,'day', true);
+    for (option in CONFIGS.days) {
+      _days += buildInput(CONFIGS.days[option],'day', false);
+    }
+
+    $(CONFIGS.days_fieldset).append(_days);
+    //Hardcoded Values
+    // '<input type="checkbox" name="day" value="Any" checked />Any<input type="checkbox" name="day" value="Monday" />M<input type="checkbox" name="day" value="Tuesday" />T<input type="checkbox" name="day" value="Wednesday" />W<input type="checkbox" name="day" value="Thursday" />Th<input type="checkbox" name="day" value="Friday" />F<input type="checkbox" name="day" value="Saturday" />Sa<input type="checkbox" name="day" value="Sunday" />Su'
+
+    //Type inputs
+    // Build the inputs
+    _types = "";
+    _types += buildInput(CONFIGS.showTypes_all,'type', true);
+    for (option in CONFIGS.showTypes) {
+      _types += buildInput(CONFIGS.showTypes[option],'type', false);
+    }
+
+    $(CONFIGS.showTypes_fieldset).append(_types);
+    //Hardcoded Values
+    // '<input type="checkbox" name="type" value="Any" checked />Any<input type="checkbox" name="type" value="solo" />Improv (Solo)<input type="checkbox" name="type" value="group" />Improv (Group)<input type="checkbox" name="type" value="standup" />Stand-up<input type="checkbox" name="type" value="sketch_s" />Sketch (Solo)<input type="checkbox" name="type" value="sketch_g" />Sketch (Group)'
+  };
+
+  buildInput = function(value, type, isChecked) {
+    var input = "";
+    /* Updated Checkboxes 
+    input += '<section title="'+buildTitle(value,type)+'">';
+    input +=  '<div class="squaredOne">';
+    input +=    '<input type="checkbox" value="'+value+'" id="'+value+'" name="'+type+'" '+((isChecked)? "checked" : "")+' />';
+    input +=    '<label for="'+value+'"></label>';
+    input +=  '</div>';
+    input += '</section>';
+    /**/
+    /* Normal Checkboxes */
+    input += '<input type="checkbox" name="'+ type +'" value="'+ value +'" '+ ((isChecked)? "checked" : "") +' />' + buildTitle(value,type);
+    /**/
+    return input;
+  };
+
+  buildTitle = function (value, type) {
+    var returnVal = "";
+
+    if (value.substr(-3) === "Any") {
+      returnVal = value.substr(-3);
+      return returnVal;
+    }
+
+    if (type === 'day') {
+      returnVal = value.substr(0,2);
+    } else {
+      _BTarr = utils.getBookingTypes();
+      returnVal = _BTarr[value];
+    }
+
+    return returnVal;
   };
 
   // changeShowDays - converts a selection of DOM elements to an array of days that 
@@ -106,7 +165,8 @@ var controls = (function() {
     } else {
       var day_arr = [];
       selected_days.each(function() {
-        if ($(this).val() === 'Any') {
+        // How should we handle?
+        if ($(this).val() === CONFIGS.days_all) {
           day_arr.push('*');
           return;
         } else {
@@ -128,7 +188,7 @@ var controls = (function() {
     } else {
       var type_arr = [];
       selected_types.each(function() {
-        if ($(this).val() === 'Any') {
+        if ($(this).val() === CONFIGS.showTypes_all) {
           type_arr.push('*');
           return;
         } else {
